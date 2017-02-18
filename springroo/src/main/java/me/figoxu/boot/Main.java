be06341d272831c0c5980527ccb4abfb8c8385a9
figoxu <xujh945@gmail.com>
2017-02-18 12:49:22 +0800

    Add Spring Boot HelloController

---
+++ springroo/src/main/java/me/figoxu/boot/Main.java
@@ -0,0 +1,16 @@
+package me.figoxu.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by figoxu on 2017/2/18.
+ */
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class,args);
+    }
+
+}
