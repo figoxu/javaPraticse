19de35095296fd625eca73db8a7e1c2bf1c9f0e0
figoxu <xujh945@gmail.com>
2017-02-21 06:41:04 +0800

    Add Ibatis Test Case

---
+++ springboot/src/test/java/me/figoxu/boot/mapper/UserMapperTest.java
@@ -0,0 +1,52 @@
+package me.figoxu.boot.mapper;
+
+import java.util.List;
+
+import me.figoxu.boot.Application;
+import me.figoxu.boot.entity.UserEntity;
+import me.figoxu.boot.enums.UserSexEnum;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(Application.class)
+public class UserMapperTest {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Test
+    public void testInsert() throws Exception {
+        userMapper.insert(new UserEntity("aa", "a123456", UserSexEnum.MAN));
+        userMapper.insert(new UserEntity("bb", "b123456", UserSexEnum.WOMAN));
+        userMapper.insert(new UserEntity("cc", "b123456", UserSexEnum.WOMAN));
+
+        Assert.assertEquals(3, userMapper.getAll().size());
+    }
+
+    @Test
+    public void testQuery() throws Exception {
+        List<UserEntity> users = userMapper.getAll();
+        if(users==null || users.size()==0){
+            System.out.println("is null");
+        }else{
+            System.out.println(users.toString());
+        }
+    }
+
+
+    @Test
+    public void testUpdate() throws Exception {
+        UserEntity user = userMapper.getOne(6l);
+        System.out.println(user.toString());
+        user.setNickName("neo");
+        userMapper.update(user);
+        Assert.assertTrue(("neo".equals(userMapper.getOne(6l).getNickName())));
+    }
+
+}
