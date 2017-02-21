c87191f022890b6d1aa372703a84252fad51103a
Figo.Xu <xujh945@qq.com>
2017-02-21 15:09:22 +0800

    fix :  test problem cause by swagger

--- springboot/src/test/java/me/figoxu/boot/mapper/UserMapperTest.java
+++ springboot/src/test/java/me/figoxu/boot/mapper/UserMapperTest.java
@@ -8,16 +8,24 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(Application.class)
+@WebAppConfiguration
+@IntegrationTest("server.port:0")
 public class UserMapperTest {
 
     @Autowired
     UserMapper userMapper;
+
+    @Value("${local.server.port}")
+    int port;
 
     @Test
     public void testInsert() throws Exception {
