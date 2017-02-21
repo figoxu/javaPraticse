4e224a16065aeaf79210f5739417179988bb730a
Figo.Xu <xujh945@qq.com>
2017-02-21 16:03:14 +0800

    add log4j configure

--- springboot/src/main/java/me/figoxu/boot/web/HelloController.java
+++ springboot/src/main/java/me/figoxu/boot/web/HelloController.java
@@ -1,15 +1,24 @@
 package me.figoxu.boot.web;
 
 import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    private Logger logger = LoggerFactory.getLogger(getClass());
 
     @ApiOperation(value="默认页面", notes="")
     @RequestMapping("/hello")
     public String index() {
+
+
+        logger.debug("Logger Level ：DEBUG");
+        logger.info("Logger Level ：INFO");
+        logger.error("Logger Level ：ERROR");
+
         return "Hello World";
     }
 
