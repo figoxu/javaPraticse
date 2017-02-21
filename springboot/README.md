867a833e6c126e76eddec71b41e86fc2d5ed49fe
Figo.Xu <xujh945@qq.com>
2017-02-21 11:58:19 +0800

    Add Swagger2 Document

--- springboot/README.md
+++ springboot/README.md
@@ -1,3 +1,4 @@
 curl http://localhost:8080/hello
 
 
+http://localhost:1111/swagger-ui.htm
--- springboot/src/main/java/me/figoxu/boot/web/HelloController.java
+++ springboot/src/main/java/me/figoxu/boot/web/HelloController.java
@@ -1,11 +1,13 @@
 package me.figoxu.boot.web;
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @ApiOperation(value="默认页面", notes="")
     @RequestMapping("/hello")
     public String index() {
         return "Hello World";
