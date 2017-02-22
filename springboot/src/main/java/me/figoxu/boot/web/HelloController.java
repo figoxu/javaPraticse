e03964b35446ecd30aa8b447433a27ee2024012a
Figo.Xu <xujh945@qq.com>
2017-02-22 10:11:42 +0800

    Spring Boot Json Response Sample

--- springboot/src/main/java/me/figoxu/boot/web/HelloController.java
+++ springboot/src/main/java/me/figoxu/boot/web/HelloController.java
@@ -1,10 +1,13 @@
 package me.figoxu.boot.web;
 
 import io.swagger.annotations.ApiOperation;
+import me.figoxu.boot.webmodel.response.HelloRespBean;
+import me.figoxu.boot.webmodel.response.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -16,7 +19,7 @@ public class HelloController {
     RestTemplate restTemplate;
 
     @ApiOperation(value="默认页面", notes="")
-    @RequestMapping("/hello")
+    @RequestMapping(value="/hello", method=RequestMethod.GET)
     public String index() {
         logger.debug("Logger Level ：DEBUG");
         logger.info("Logger Level ：INFO");
@@ -25,5 +28,20 @@ public class HelloController {
         logger.info(resp);
         return "Hello World";
     }
+
+
+    @ApiOperation(value="默认页面", notes="")
+    @RequestMapping(value="/helloJson", method=RequestMethod.GET)
+    public Response jsonSample() {
+        HelloRespBean respBean = new HelloRespBean();
+        respBean.setFoo("Foo String");
+        respBean.setBar(1024);
+        respBean.setMoney(100000000.0f);
+        Response response = new Response();
+        response.setContent(respBean);
+        response.setCode(200);
+        return response;
+    }
+
 
 }
